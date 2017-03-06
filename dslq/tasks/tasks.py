6e7a29ff3dc791f99a6e2b1c5c7fe652c3ea9e1e
Mark Stacy <mbstacy@gmail.com>
2017-03-06 11:56:50 -0600

    updated index data and remove direct db manipulation. This accomplished through api

--- dslq/tasks/tasks.py
+++ dslq/tasks/tasks.py
@@ -19,7 +19,7 @@ from urlparse import urlparse, parse_qs
 from bs4 import BeautifulSoup
 from xmltodict import parse
 from subprocess import call
-
+import ConfigParser
 
 #Default base directory 
 basedir="/data/web_data/static"
@@ -30,6 +30,25 @@ filterLinks=[]
 s= requests.session()
 modsURL_template =  "https://www.gpo.gov/fdsys/pkg/{0}/mods.xml"
 
+
+def _get_config_parameter(group,param,config_file="cybercom.cfg"):
+    config = ConfigParser.ConfigParser()
+    config.read(config_file)
+    return config.get(group,param)
+def _api_get(tag,collection='inventory'):
+    base_url = _get_config_parameter('api','base_url')
+    query= "{'filter':{'TAG':'%s'}}" % (tag)
+    api_url = "{0}/api-dsl/data_store/data/congressional/{1}/.json?query={2}".format(base_url,collection,query)
+    req =requests.get(api_url)
+    return req.json()
+def _api_save(data,collection='inventory'):
+    token = _get_config_parameter('api','token')
+    base_url = _get_config_parameter('api','base_url')
+    headers ={"Content-Type":"application/json","Authorization":"Token {0}".format(token)}
+    api_url = "{0}/api-dsl/data_store/data/congressional/{1}/.json".format(base_url,collection)
+    req = requests.post(api_url,data=json.dumps(data),headers=headers)
+    req.raise_for_status()
+    return True
 #Example task
 @task()
 def add(x, y):
@@ -41,73 +60,29 @@ def add(x, y):
     return result
     
 @task()
-def sub(x, y):
-    """ Example task that subtracts two numbers
-        args: x and y
-        return subtraction
-    """
-    result = x - y
-    return result
-
-@task()
-def get_cong_data_python3(start="99",end="115"):
+def get_cong_data_python3(start="99",end="116"):
     """Python 3 script to pull congressional hearings from gpo website.
        args: 
        kwargs: start="99" - starting congressional session (string)
-               end="115" - end congressional session default one more then wanted max 114 so default is 115 (string)
+               end="116" - end congressional session default one more then wanted max 115 so default is 116 (string)
     """
     task_id = str(get_cong_data_python3.request.id)
     #create Result Directory
     resultDir = os.path.join(basedir, 'dsl_tasks/', task_id)
     os.makedirs(resultDir)
-    call(["/anaconda3/gpo/mods.py",start,end,"{0}/log.txt".format(resultDir)])
-    return "http://dev.libraries.ou.edu/dsl_tasks/{0}".format(task_id)
+    call(["/anaconda3/gpo/mods.py",start,end,"{0}/log.txt".format(resultDir),_get_config_parameter('api','token')])
+    return "{0}/dsl_tasks/{1}".format(_get_config_parameter('api','base_url'),task_id)
     
 @task()
-def get_congressional_data(congress=None, mongo_database="congressional",mongo_collection="hearings",update=None):
-    """Congressional Hearing Inventory task
-        Agrs: 
-        kwargs: congress=<None> # This will run all congresses. Valid values is 99-114 to run individual 
-                mongo_database=<'congressional'>, 
-                mongo_collection=<'hearings'>, 
-                update=<None> # this will run through all hearings
-        If update = False will inventory entire congressional hearins. Must delete records in mongo. Task does not check of record exists.
-    """
-    # Session will work better to store connection state. Cookies!
-    s = requests.Session()
-    #load base page and setup session
-    s.get("https://www.gpo.gov/fdsys/")
-    url_template="http://www.gpo.gov/fdsys/search/search.action?sr={0}&originalSearch=collection:CHRG&st=collection:CHRG&ps=100&na=__congressnum&se=__{1}true&sb=dno&timeFrame=&dateBrowse=&govAuthBrowse=&collection=&historical=true"
-    total_ids=[]
-    db = MongoClient("dsl_search_mongo",27017)
-    #db.congressional.hearings.save(x)
-    if update:
-        """ Just pull first page of congressional hearing search"""
-        if congress:
-            #for cong in range(99,115):
-            total_ids =total_ids + get_chrg_ids(s,url_template,page=1,congress=congress)
-        else:   
-            for cong in range(99,115):
-                total_ids =total_ids + get_chrg_ids(s,url_template,page=1,congress=cong)
-        print "Update Total IDs returned %d" % (len(total_ids))
-        for chrg in total_ids:
-            if db[mongo_database][mongo_collection].find({'tag':chrg}).count() < 1:
-                modsParser(s,chrg,modsURL_template.format(chrg))
-    else:  
-        """ Run entire inventory """
-        if congress:
-            total_ids =total_ids + get_ids(s,url_template,congress)
-        else:
-            for cong in range(99,115):
-                total_ids =total_ids + get_ids(s,url_template,congress)
-        print "All Total IDs returned %d" % (len(total_ids))
-        for chrg in total_ids:
-            if db[mongo_database][mongo_collection].find({'tag':chrg}).count() < 1:
-                modsParser(s,chrg,modsURL_template.format(chrg))
-
-
-@task()
 def search_stats(index,doctype,query,context_pages=5):
+    """
+    Search results returned in csv and pickled dataframe!
+    args:
+        index - Elasticsearch Index
+        doctype - Elasticsearch Doctype
+        query - Elasticsearch query
+        context_pages - Default 5 (Number of lines above and below
+    """
     task_id = str(search_stats.request.id)
     #create Result Directory
     resultDir = os.path.join(basedir, 'dsl_tasks/', task_id)
@@ -119,10 +94,18 @@ def search_stats(index,doctype,query,context_pages=5):
     df.to_csv("{0}/es_query_data.csv".format(resultDir),encoding='utf-8')
     #save dataframe pickle to file
     df.to_pickle("{0}/es_query_data.pkl".format(resultDir))
-    return "http://dev.libraries.ou.edu/dsl_tasks/{0}".format(task_id)
+    return "{0}/dsl_tasks/{1}".format(_get_config_parameter('api','base_url'),task_id)
 
 @task()
 def search_main_stats(index,doctype,query,context_pages=5):
+    """
+    Search results returned in csv and pickled dataframe!
+    args:
+        index - Elasticsearch Index
+        doctype - Elasticsearch Doctype
+        query - Elasticsearch query
+        context_pages - Default 5 (Number of lines above and below
+    """
     task_id = str(search_main_stats.request.id)
     #create Result Directory
     resultDir = os.path.join(basedir, 'dsl_main_tasks/', task_id)
@@ -134,100 +117,57 @@ def search_main_stats(index,doctype,query,context_pages=5):
     df.to_csv("{0}/es_query_data.csv".format(resultDir),encoding='utf-8')
     #save dataframe pickle to file
     df.to_pickle("{0}/es_query_data.pkl".format(resultDir))
-    return "http://dev.libraries.ou.edu/dsl_main_tasks/{0}".format(task_id)
+    return "{0}/dsl_tasks/{1}".format(_get_config_parameter('api','base_url'),task_id)
 
 @task()
-def index_data(inventory_option=None): # "inventory_option":"YES"
-    db = MongoClient("dsl_search_mongo",27017)
-    testURL="https://dev.libraries.ou.edu/api-dsl/data_store/data/congressional/hearings/?format=json"
-    r = s.get(testURL)
+def index_data(): # "inventory_option":"YES"
+    """
+    Elasticsearch Index data: Runs through all recorded hearings within congressional hearings collection.
+    """
+    api_host=_get_config_parameter('api','base_url')
+    URL="{0}/api-dsl/data_store/data/congressional/hearings/?format=json".format(api_host)
+    r = s.get(URL)
     rjson = r.json()
     pagecount = rjson['meta']['pages']
-    url_template="https://dev.libraries.ou.edu/api-dsl/data_store/data/congressional/hearings/.json?page={0}"
-
-    if inventory_option:
-        es_delete_all("congressional",Elasticsearch(ES_HOST))
-        db.congressional.inventory.remove({})
+    url_template="{0}/api-dsl/data_store/data/congressional/hearings/.json?page={1}"
 
     for i in range(1,pagecount+1):
-        rd=requests.get(url_template.format(i))
+        rd=requests.get(url_template.format(api_host,i))
         rdjson=rd.json()
         for item in rdjson['results']:
-            if not inventory_option:
-                if db.congressional.inventory.find({'TAG':item['TAG'],'STATUS':"FAIL"}).count()>0:
-                    htmlparser(item)
-
-                elif db.congressional.inventory.find({'TAG':item['TAG']}).count() == 0:
-                    htmlparser(item)
+            #metadata = htmlparser(item)
+            data=_api_get(item["TAG"])
+            if data['count']>0:
+                if data['results'][0]['STATUS'] != 'SUCCESS':
+                    metadata = htmlparser(item)
+                    metadata['_id']=data['results'][0]['_id']
+                    _api_save(metadata)
             else:
-                htmlparser(item)
-
+                _api_save(htmlparser(item))
+    
+            
 @task()
 def add_chamber():
     es_add_chamber("congressional","hearings",Elasticsearch(ES_HOST))
 
 def es_retun_all(es,query,index,doctype,context_pages):
-    #meta = es_search(es, index, doctype, query=query, page=1, nPerPage=1)
-
+    """
+    Returns all results from search to return results 
+    """
     result = es_helper_scan(es,index,doctype,query,context_pages)
-
     return result
 
-
-def get_chrg_ids(s,url_template,page=1,congress=99):
-    try:
-        r=s.get(url_template.format(page,congress))
-        soup=BeautifulSoup(r.text,'html.parser')
-    except:
-        sleep(15)
-        r=s.get(url_template.format(page,congress))
-        soup=BeautifulSoup(r.text,'html.parser')
-    links=[]
-    for link in soup.findAll('a'):
-        if link.get('href'):
-            links.append(link.get('href'))
-    print "Total Links: %d" % (len(links))
-    #print links
-    valid_ids=[]
-    for link in links:
-        end_url=link.split('/')[-1]
-        if re.match('^CHRG*',end_url):
-            valid_ids.append(end_url.split('.p')[0])
-    print "Total Valid Links: %d" % (len(valid_ids))
-    return valid_ids
-
-def get_ids(s,url_template,congress):
-    cum_ids=[]
-    for itm in range(1,1000):
-        ids = get_chrg_ids(s,url_template,page=itm,congress=congress)
-        if ids==[]:
-            break
-        cum_ids = cum_ids + ids
-    return cum_ids
-
-
-def modsParser(s,tag,url):
-    xmlURL = url
-    r = s.get(xmlURL)
-    try:
-        data = parse(r.text)
-        data["tag"]=tag
-        x = json.loads(json.dumps(data).replace("@",'').replace("#",''))
-        db = MongoClient("dsl_search_mongo",27017)
-        db.congressional.srihearings.save(x)
-    except:
-        print "ERROR: %s %s" %  (tag,url) 
-
-
 def htmlparser(x):
-    db = MongoClient("dsl_search_mongo",27017)
+    """
+    Function to parse metadata and return text from gpo site and index into Elasticsearch.
+    """
     line_count=0
     rurls=requests.session()
     url=""
     tag=""
     try:
+        #set metadata
         if type(x['HELD_DATE'])== list:
-            # print x['HELD_DATE'][1]
             helddate=x['HELD_DATE'][-1]
         else:
             helddate=x['HELD_DATE']
@@ -236,22 +176,16 @@ def htmlparser(x):
         url = x['URL']
         title = x['TITLE_INFO'][0]['title']
         title = title.replace("&amp;","&").replace("&quot;",'"')\
-                                              .replace("&apos;","'").replace("&gt;",">").replace("&lt;","<")
-
-
-
+                    .replace("&apos;","'").replace("&gt;",">").replace("&lt;","<")
+        
         hd = datetime(int(helddate.split('-')[0]),int(helddate.split('-')[1]),int(helddate.split('-')[2]))
         # print hd
         h_date = '{dt:%B} {dt.day}, {dt.year}'.format(dt=hd)
-
-
-        # print "TITLE : ",title," DATE : ",h_date.upper()," URL : ",url,"TAG : ",tag
+        # check if url of text is available
         if url=="":
-            # print json.dumps({'TAG':tag,'LINE_COUNT': 'N/A','TYPE': 'PDF','STATUS':'FAIL'})
-            db.congressional.inventory.save({'TAG':tag,'LINE_COUNT': 'N/A','TYPE': 'PDF','STATUS':'FAIL'})
-            return
-
-
+            #db.congressional.inventory.save({'TAG':tag,'LINE_COUNT': 'N/A','TYPE': 'PDF','STATUS':'FAIL'})
+            return {'TAG':tag,'LINE_COUNT': 'N/A','TYPE': 'PDF','STATUS':'FAIL'}
+        # Get text from GPO url
         try:
             soup = BeautifulSoup(rurls.get(url).text,'html.parser')
         except:
@@ -271,51 +205,34 @@ def htmlparser(x):
         line_count=len(requiredDataList)
 
         if line_count < 10:
-            # print json.dumps({'TAG':tag,'LINE_COUNT': line_count,'TYPE': 'PDF','STATUS':'FAIL'})
-            metadata=db.congressional.inventory.find_one({'TAG':tag})
-            if metadata:
-                metadata['LINE_COUNT'] = line_count
-                metadata['TYPE']="PDF"
-                metadata['STATUS']="FAIL"
-                db.congressional.inventory.save(metadata)
-            else:
-                db.congressional.inventory.save({'TAG':tag,'LINE_COUNT': line_count,'TYPE': 'PDF','STATUS':'FAIL'})
-
+            return {'TAG':tag,'LINE_COUNT': line_count,'TYPE': 'PDF','STATUS':'FAIL'}
         else:
             es=Elasticsearch(ES_HOST)
             try:
-                temp=es.count(index="congressional", doc_type="hearings")
-                id=temp['count'] + 1
+                req =es.search(index='congressional',doc_type='hearings',body={"query":{"match_all":{}},"sort":{"SENTENCE_ID": "desc"},"size":1})
+                #add one to the largest id 
+                id =  req['hits']['hits'][0]['_source']['SENTENCE_ID'] + 1
             except:
                 id=1
 
             if es_index_exist("congressional",Elasticsearch(ES_HOST)):
-                es_delete_by_tag("congressional","hearings",tag,Elasticsearch(ES_HOST))
-
-            for each_sentence in requiredDataList:
-                data={'TAG': tag,'DATA': each_sentence, 'TITLE': title,'DATE':helddate}
-                es_insert("congressional","hearings",data,es,id)
-                id+=1
-                # print json.dumps({'TAG':tag,'LINE_COUNT': line_count,'TYPE': 'TEXT','STATUS':'SUCCESS'})
-
-            metadata=db.congressional.inventory.find_one({'TAG':tag})
-            if metadata:
-                metadata['LINE_COUNT'] = line_count
-                metadata['TYPE']="TEXT"
-                metadata['STATUS']="SUCCESS"
-                db.congressional.inventory.save(metadata)
+                req =es.search(index='congressional',doc_type='hearings',body={'query':{'match_phrase':{'TAG':{'query':tag,'type':'phrase'}}}},size=0)
+                if req['hits']['total'] != line_count:
+                    #Delete tag from index
+                    es_delete_by_tag("congressional","hearings",tag,Elasticsearch(ES_HOST))
+                    #Insert complete sentences into index
+                    for each_sentence in requiredDataList:
+                        data={'TAG': tag,'DATA': each_sentence, 'TITLE': title,'DATE':helddate}
+                        es_insert("congressional","hearings",data,es,id)
+                        id+=1
             else:
-                db.congressional.inventory.save({'TAG':tag,'LINE_COUNT': line_count,'TYPE': 'TEXT','STATUS':'SUCCESS'})
+                #Insert complete sentences into index
+                for each_sentence in requiredDataList:
+                    data={'TAG': tag,'DATA': each_sentence, 'TITLE': title,'DATE':helddate}
+                    es_insert("congressional","hearings",data,es,id)
+                    id+=1
+            return {'TAG':tag,'LINE_COUNT': line_count,'TYPE': 'TEXT','STATUS':'SUCCESS'}
 
     except Exception as e:
-        metadata=db.congressional.inventory.find_one({'TAG':tag})
-        if metadata:
-            metadata['LINE_COUNT'] = line_count
-            metadata['TYPE']="ERROR"
-            metadata['STATUS']="FAIL"
-            metadata['ERROR']=str(e)
-            metadata['URL']=url
-            db.congressional.inventory.save(metadata)
-        else:
-            db.congressional.inventory.save({'TAG':tag,'LINE_COUNT': line_count,'TYPE': 'ERROR','STATUS':'FAIL','ERROR':str(e),'URL':url})
+        return {'TAG':tag,'LINE_COUNT': line_count,'TYPE': 'ERROR','STATUS':'FAIL','ERROR':str(e),'URL':url}
     rurls.close()
